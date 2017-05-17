734e1de75c0158725ba4341804f6b067c5abcaff
Simon Tranter <simon.tranter@prospectsoft.com>
2017-05-17 14:56:46 +0100

    Updated packages and config

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 module.exports = {
     entry: {
         test: "./test/index.ts"
     },
     output: {
-        path: "./test",
+        path: path.resolve("./test"),
         filename: "bundle.js"
     },
     resolve: {
